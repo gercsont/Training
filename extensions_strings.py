e4eb55f0606af94084a0e5ad674ab8cd340332f0
gercsont <gregorius.osseus@gmail.com>
2022-05-09 16:53:29 +0200

    első napi

---
+++ extensions_strings.py
@@ -0,0 +1,22 @@
+name = "John Doe"
+print("Hello " + name + "!")
+
+print(2*name)
+
+index = 5
+print(type(index))
+
+print("Ez az " + str(index) + ". könyv.")
+
+print("Ez az ", index, ". album.", sep="")
+
+print(f"Ez az {index}. lemez.")
+
+digit = "6"
+print(digit)
+digit +="8"
+print(digit)
+digit +=str(9)
+print(digit)
+
+print(2 ** 10)
---
+++ input.py
@@ -0,0 +1,11 @@
+name = input("Mi a neved? ")
+print("Hello, ", name, "!", sep="")
+
+szam = input("Mi a kedvenc számod? ")
+szam = int(szam)
+print("A kedvenc számod: ", szam, ".", sep="")
+print("A kedvenc számod duplája: ", szam + szam, ".", sep="")
+
+szam = int(input("Mi a kedvenc számod? "))
+print("A kedvenc számod: " + str(szam) + ".")
+print("A kedvenc számod duplája: " + str(szam*2) + ".")
