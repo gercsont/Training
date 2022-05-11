53e914122d029a1a3bde9c8635c075b4cc754891
gercsont <gregorius.osseus@gmail.com>
2022-05-11 16:33:53 +0200

    tesztek

--- functions_demo.py
+++ functions_demo.py
@@ -3,29 +3,12 @@ from random import randint
 
 def is_not_negative(number):
     return number >= 0
-print(is_not_negative(-10))
-
-if __name__ == "__main__":
-    
-print ("""
-VÉGE
-""")
 
 def get_circle_area(number):
     return number ** 2 * 3.14
-print(get_circle_area(2))
-
-print ("""
-VÉGE
-""")
 
 def get_rectangle_area(A, B):
     return A * B
-print(get_rectangle_area(5, 4))
-
-print ("""
-VÉGE
-""")
 
 def absolute(number):
     if number > 0:
@@ -34,31 +17,16 @@ def absolute(number):
         return number * -1
     else:
         return 0
-print(absolute(-564))
-
-print ("""
-VÉGE
-""")
 
 def sum_numbers(list):
     sum = 0
     for n in list:
         sum += n
     return sum
-list = [1, 2, 3, -4, -5, 6, 7, 8, 9]
-print(sum_numbers(list))
 
-print ("""
-VÉGE
-""")
 
 def roll_dice():
     return randint(1,6)
-print(roll_dice())
-
-print ("""
-VÉGE
-""")
 
 def count_positive_numbers(list):
     sum = 0
@@ -66,5 +34,22 @@ def count_positive_numbers(list):
         if n > 0:
             sum += 1
     return sum
-list = [-1, -2, -3, 4, 5, -6, -7, 8, -9]
-print(count_positive_numbers(list))
+
+
+if __name__ == "__main__":
+
+    print(is_not_negative(10))
+
+    print(get_circle_area(10))
+
+    print(get_rectangle_area(200, 50))
+
+    print(absolute(-789))
+    
+    list = [5, 25, 50, 150]
+    print(sum_numbers(list))
+
+    print(roll_dice())
+    
+    list = [1, 5, 5, -6, -8]
+    print(count_positive_numbers(list))
--- funkcióteszt_test.py
+++ funkcióteszt_test.py
@@ -1,7 +1,27 @@
-from functions_demo import is_not_negative
+from functions_demo import is_not_negative, absolute
 
 
 def test_with_negative_number():
     number = -10
     result = is_not_negative(number)
     assert result == False
+
+def test_with_positive_number():
+    number = 10
+    result = is_not_negative(number)
+    assert result == True
+
+def test_with_zero():
+    number = 0
+    result = is_not_negative(number)
+    assert result == True
+
+def test_absolute_with_positive():
+    number = 5
+    result = absolute(number)
+    assert result == 5
+
+def test_absolute_with_negative():
+    number = -5
+    result = absolute(number)
+    assert result == 5
